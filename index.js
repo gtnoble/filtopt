87c7c96df91cdccadcc2d52a735d6a5dc1a64664
gtnoble <garretnoble@gmail.com>
2023-12-26 08:44:33 -0600

    Removed vestigial index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-import math from 'mathjs';
-
-const cauerLadder = (...lSections) => {
-    let combinedAbcd = math.identity(2)
-    for (const lSection of lSections) {
-        combinedAbcd = math.multiply(combinedAbcd, lSection(angularFrequency))
-
-    }
-}
